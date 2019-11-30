c8dfc143293635f66cbebe737cb2f4c6b99cba49
Gad <gadkh2@gmail.com>
2019-12-01 01:53:05 +0200

    first commit

---
+++ src/main/java/gadZoo/GadZooApplication.java
@@ -0,0 +1,13 @@
+package gadZoo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GadZooApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GadZooApplication.class, args);
+	}
+
+}
---
+++ src/test/java/gadZoo/GadZooApplicationTests.java
@@ -0,0 +1,13 @@
+package gadZoo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GadZooApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
