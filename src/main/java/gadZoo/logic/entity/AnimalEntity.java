7c988d7783ae8ea8f02b69fe50a58638f783d7a9
Gad <gadkh2@gmail.com>
2019-12-01 02:03:38 +0200

    Create an AnimalEntity class
    An AnimalEntity class is the object that insert into DB as aa row.

--- src/main/java/gadZoo/logic/entity/AnimalEntity.java
+++ src/main/java/gadZoo/logic/entity/AnimalEntity.java
@@ -1,5 +1,61 @@
 package gadZoo.logic.entity;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
+@Entity
+@Table(name="Animals")
 public class AnimalEntity {
+	private String id;
+	private String name;
+	private String type;
+	private String age;
+	private String food;
+	
+	public AnimalEntity() {
+		super();
+	}
 
+	@Id
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+		this.setId(this.type+"@@"+this.name);
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+		this.setId(this.type+"@@"+this.name);
+	}
+
+	public String getAge() {
+		return age;
+	}
+
+	public void setAge(String age) {
+		this.age = age;
+	}
+
+	public String getFood() {
+		return food;
+	}
+
+	public void setFood(String food) {
+		this.food = food;
+	}
 }
